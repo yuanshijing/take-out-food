ab64ac69db57135ae6fb5d0b9743f44a2ec00a6c
yuanshijing <826774822@qq.com>
2017-08-25 19:05:55 +0800

    'complete'

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,9 +1,11 @@
-describe('Take out food', function () {
+const bestCharge= require('../src/best-charge');
 
-  it('should generate best charge when best is 指定菜品半价', function() {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+describe('Take out food', function() {
+
+    it('should generate best charge when best is 指定菜品半价', function() {
+        let inputs = ['ITEM0001 x 1', 'ITEM0013 x 2', 'ITEM0022 x 1'];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 黄焖鸡 x 1 = 18元
 肉夹馍 x 2 = 12元
@@ -13,14 +15,14 @@ describe('Take out food', function () {
 指定菜品半价(黄焖鸡，凉皮)，省13元
 -----------------------------------
 总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected);
+    });
 
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+    it('should generate best charge when best is 满30减6元', function() {
+        let inputs = ['ITEM0013 x 4', 'ITEM0022 x 1'];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
 凉皮 x 1 = 8元
@@ -29,20 +31,20 @@ describe('Take out food', function () {
 满30减6元，省6元
 -----------------------------------
 总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected);
+    });
 
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+    it('should generate best charge when no promotion can be used', function() {
+        let inputs = ['ITEM0013 x 4'];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
 -----------------------------------
 总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected);
+    });
 
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,79 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+const loadAllItems = require('./items');
+const loadPromotions = require('./promotions');
+
+function makeItemsList(inputs) {
+    let allItems = loadAllItems();
+    let itemsInfo = [];
+    inputs.forEach((item) => {
+        itemsInfo.push({id:item.split(' x ')[0],count:item.split(' x ')[1]});
+    })
+    itemsInfo.forEach((i) => {
+        for (let item of allItems) {
+            if (i.id == item.id) {
+                i.name = item.name;
+                i.price = item.price;
+                i.sum = i.price * i.count;
+            }
+        }
+    })
+    return itemsInfo;
+}/*[ { id: 'ITEM0013', count: '4', name: '肉夹馍', price: 6, sum: 24 },
+  { id: 'ITEM0022', count: '1', name: '凉皮', price: 8, sum: 8 } ]*/
+
+function promotion1(itemsInfo) {
+    let summary = {sum:0,save:0};
+    itemsInfo.forEach((i) => summary.sum+=i.sum);
+    if (summary.sum >= 30){
+        summary.save = 6;
+        summary.sum -= 6;
+    }
+    return summary;
+}//{ sum: 26, save: 6 }
+
+function promotion2(itemsInfo) {
+    let summary = {sum:0,save:0,name:[]};
+    let promotions = loadPromotions()[1].items;
+    itemsInfo.forEach((item) =>{
+        summary.sum += item.sum;
+        if(promotions.includes(item.id)) {
+            summary.save += item.sum/2;
+            summary.name.push(item.name);
+        }
+    })
+    summary.sum -= summary.save;
+    return summary;
+}//{ sum: 25, save: 13, name: [ '黄焖鸡', '凉皮' ] }
+
+function printReceipt(sum1,sum2,itemsInfo) {
+    let sum = 0;
+    let receipt = '============= 订餐明细 =============\n';
+    itemsInfo.forEach((item) =>receipt += `${item.name} x ${item.count} = ${item.sum}元\n`);
+    if(sum1.save !== 0 || sum2.save !== 0) {
+        receipt += '-----------------------------------\n使用优惠:\n';
+        if(sum2.sum > sum1.sum)  {
+            receipt += `满30减6元，省6元\n`;
+            sum = sum1.sum;
+        }else {
+            receipt += `指定菜品半价(${sum2.name[0]}`;
+            for (let i = 1;i<sum2.name.length;i++) {
+                receipt += `，${sum2.name[i]}`;
+            }
+            receipt += `)，省${sum2.save}元\n`;
+            sum = sum2.sum;
+        }
+    }else{
+        sum = sum1.sum;
+    }
+    receipt += `-----------------------------------
+总计：${sum}元
+===================================`;
+    return receipt;
 }
+
+module.exports = function bestCharge (inputs) {
+    let itemsInfo = makeItemsList(inputs);
+    let sum1 = promotion1(itemsInfo);
+    let sum2 = promotion2(itemsInfo);
+    return printReceipt(sum1,sum2,itemsInfo);
+}
+
